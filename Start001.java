05246a26acade628f0f648a680b24f4fc1c8f186
Oleksiy Korniyenko <korniyenkooleksiy@gmail.com>
2025-03-17 15:46:25 +0100

    Change Start001.java

--- Start001.java
+++ Start001.java
@@ -1,6 +1,6 @@
 public class Start001 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!Hello World2!!!");
+        System.out.println("Hello bad world");
     }
 }
